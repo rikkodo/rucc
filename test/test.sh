106c6a7fb777cce87e99d6094014e823b730e2bf
rikkodo <ctrlalt.exit@gmail.com>
2020-02-18 23:24:20 +0900

    Step1. 自動テストの作成

---
+++ test/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+try () {
+    expected="$1"
+    input="$2"
+
+    cargo run -- "$input" > tmp.s
+    gcc -o tmp tmp.s
+    ./tmp
+    actual="$?"
+    if [ "$actual" = "$expected" ] ; then
+        echo "$input => $actual"
+    else
+        echo "$input => $expected expected, but got $actual"
+    fi
+    rm ./tmp.s ./tmp
+}
+
+try 0 0
+try 42 42
