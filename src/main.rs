8d4bda5e9e0f57f5d24edcb828f2a8666dd02936
rikkodo <ctrlalt.exit@gmail.com>
2020-02-18 21:42:58 +0900

    Step1. コンパイラ本体の作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+fn run_app() -> Result<(), String> {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        return Err("Too few arguments.".to_string());
+    }
+
+    let val: u8 = match args[1].parse() {
+        Ok(num) => num,
+        Err(_) => return Err("Parse error ".to_string() + &args[1]),
+    };
+
+    println!(".intel_syntax noprefix");
+    println!(".global main");
+    println!("main:");
+    println!("    mov rax, {}", val);
+    println!("    ret");
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    std::process::exit(match run_app() {
+        Ok(_) => 0,
+        Err(err) => {
+            eprintln!("error: {:?}", err);
+            1
+        }
+    });
 }
