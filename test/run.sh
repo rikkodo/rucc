2606676ee822f5b505cb8fffceaecdfa78b76bc2
rikkodo <ctrlalt.exit@gmail.com>
2020-02-19 22:25:48 +0900

    実行用スクリプト追加

---
+++ test/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR=$(dirname $0)
+DEBUGPATH="${DIR}/../target/debug/"
+MODULE="rucc"
+
+eval "${DEBUGPATH}${MODULE} ${1} > tmp.s" &&\
+gcc -o tmp tmp.s &&
+./tmp; echo $?
+
+rm -f tmp.s tmp
