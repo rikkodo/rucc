59e2c93b1d61f81f08f80e7a767e2882974802c5
rikkodo <ctrlalt.exit@gmail.com>
2020-02-26 21:57:36 +0900

    空白があった際の挙動を訂正

--- test/run.sh
+++ test/run.sh
@@ -4,7 +4,7 @@ DIR=$(dirname $0)
 DEBUGPATH="${DIR}/../target/debug/"
 MODULE="rucc"
 
-eval "${DEBUGPATH}${MODULE} ${1} > tmp.s" &&\
+eval "${DEBUGPATH}${MODULE} \"${1}\" > tmp.s" &&\
 gcc -o tmp tmp.s &&
 ./tmp; echo $?
 
