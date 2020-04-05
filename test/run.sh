49bbf7ff20d0827e928f4e772de52345ad9099f8
rikkodo <ctrlalt.exit@gmail.com>
2020-04-05 12:22:01 +0900

    アセンブラを標準エラーに出力するオプション

--- test/run.sh
+++ test/run.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-DIR=$(dirname $0)
+DIR=$(dirname $0 [-p: print])
 DEBUGPATH="${DIR}/../target/debug/"
 MODULE="rucc"
+PRINT=""
+
+while :
+do
+    case $1 in
+        -p) PRINT="1"; shift;;
+        *)  break;;
+    esac
+done
 
 eval "${DEBUGPATH}${MODULE} \"${1}\" > tmp.s" &&\
-gcc -o tmp tmp.s &&
+gcc -o tmp tmp.s &&\
 ./tmp; echo $?
+
+if [ "${PRINT}" != "" ] ;
+then
+    cat tmp.s 1>&2
+fi
 
 rm -f tmp.s tmp
